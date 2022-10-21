741da5397b9f0bad363136374b86d17d277cff92
Mohsin <Mohsin@MOHSINALI>
2022-10-22 01:29:30 +0500

    Add project files.

---
+++ kitaab ghar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kitaab_ghar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
